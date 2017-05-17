2080d0f4276181c21a9392de9e4d867d048afb6f
Samip Shah <samipshah86@gmail.com>
2017-05-17 15:32:08 -0700

    first commit with video

--- video.py
+++ video.py
@@ -1,5 +1,6 @@
 from moviepy.editor import ImageSequenceClip
 import argparse
+import glob
 
 
 def main():
@@ -17,9 +18,11 @@ def main():
         help='FPS (Frames per second) setting for the video.')
     args = parser.parse_args()
 
+    files = glob.glob(args.image_folder + "/*.jpg")
+    files = sorted(files, key=lambda x: int(x.split("/")[-1].split('.')[0]))
     video_file = args.image_folder + '.mp4'
     print("Creating video {}, FPS={}".format(video_file, args.fps))
-    clip = ImageSequenceClip(args.image_folder, fps=args.fps)
+    clip = ImageSequenceClip(files, fps=args.fps)
     clip.write_videofile(video_file)
 
 
